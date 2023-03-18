b13d9415595a6341abe1e90342979df5c27951e3
pmfoysal <pmfoysal@gmail.com>
2023-03-18 13:52:25 +0600

    Books API Slice Added

---
+++ src/redux/features/books.js
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const books = createApi({
+   reducerPath: 'books',
+   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9000' }),
+   tagTypes: ['books', 'book'],
+   endpoints: builder => ({
+      getBooks: builder.query({
+         query: () => '/books',
+         providesTags: ['books'],
+      }),
+      getBook: builder.query({
+         query: id => `/books/${id}`,
+         providesTags: (...args) => [{ type: 'book', id: args[2] }],
+      }),
+      addBook: builder.mutation({
+         query: data => ({
+            url: '/books',
+            method: 'POST',
+            body: data,
+         }),
+         invalidatesTags: ['books'],
+      }),
+      editBook: builder.mutation({
+         query: ({ id, ...data }) => ({
+            url: `/books/${id}`,
+            method: 'PATCH',
+            body: data,
+         }),
+         invalidatesTags: (...args) => [{ type: 'book', id: args[2].id }],
+      }),
+      deleteBook: builder.mutation({
+         query: id => ({
+            url: `/books/${id}`,
+            method: 'DELETE',
+         }),
+         invalidatesTags: (...args) => ['books', { type: 'book', id: args[2] }],
+      }),
+   }),
+});
+
+export default books;
+export const { useGetBooksQuery, useGetBookQuery, useAddBookMutation, useEditBookMutation, useDeleteBookMutation } = books;
