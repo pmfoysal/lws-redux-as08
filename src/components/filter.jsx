dbe93cfc7a0363ba065147340d30e17db09c4793
pmfoysal <pmfoysal@gmail.com>
2023-03-18 13:14:27 +0600

    Filter Component Added

---
+++ src/components/filter.jsx
@@ -0,0 +1,11 @@
+export default function Filter() {
+   return (
+      <div class='flex items-center justify-between mb-12'>
+         <h4 class='mt-2 text-xl font-bold'>Book List</h4>
+         <div class='flex items-center space-x-4'>
+            <button class='lws-filter-btn active-filter'>All</button>
+            <button class='lws-filter-btn'>Featured</button>
+         </div>
+      </div>
+   );
+}
