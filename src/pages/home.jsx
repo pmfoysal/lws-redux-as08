c00beb4c4a98fc6bce9248d77c34921cc630e933
pmfoysal <pmfoysal@gmail.com>
2023-03-18 13:17:53 +0600

    Home Page Component Added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,15 @@
+import Book from '../components/book';
+import Filter from '../components/filter';
+
 export default function Home() {
-   return <div>Home</div>;
+   return (
+      <main class='py-12 px-6 2xl:px-6 container'>
+         <div class='order-2 xl:-order-1'>
+            <Filter />
+            <div class='space-y-6 md:space-y-0 md:grid grid-cols-1 lg:grid-cols-3 gap-6'>
+               <Book />
+            </div>
+         </div>
+      </main>
+   );
 }
