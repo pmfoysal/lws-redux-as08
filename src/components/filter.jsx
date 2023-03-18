75d289541dd99cd5969d55282c7de45efba61eee
pmfoysal <pmfoysal@gmail.com>
2023-03-18 21:33:27 +0600

    Book Filter Button Functionality Added

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,10 +1,29 @@
+import { setFilter } from '../redux/features/others';
+import { useDispatch, useSelector } from 'react-redux';
+
 export default function Filter() {
+   const dispatch = useDispatch();
+   const { filter } = useSelector(store => store.others);
+
+   function handleFilter(value) {
+      dispatch(setFilter(value));
+   }
+
+   function getColor(value) {
+      if (filter === value) return 'active-filter';
+      return '';
+   }
+
    return (
       <div className='flex items-center justify-between mb-12'>
          <h4 className='mt-2 text-xl font-bold'>Book List</h4>
          <div className='flex items-center space-x-4'>
-            <button className='lws-filter-btn active-filter'>All</button>
-            <button className='lws-filter-btn'>Featured</button>
+            <button className={`lws-filter-btn ${getColor('all')}`} onClick={() => handleFilter('all')}>
+               All
+            </button>
+            <button className={`lws-filter-btn ${getColor('featured')}`} onClick={() => handleFilter('featured')}>
+               Featured
+            </button>
          </div>
       </div>
    );
