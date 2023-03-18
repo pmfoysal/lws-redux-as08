4130ce44032609ef124be766bd11ef9759bb9de4
pmfoysal <pmfoysal@gmail.com>
2023-03-18 13:20:44 +0600

    Add Page Component Added

--- src/pages/add.jsx
+++ src/pages/add.jsx
@@ -1,3 +1,14 @@
+import Form from '../components/form';
+
 export default function Add() {
-   return <div>Add</div>;
+   return (
+      <main class='py-6 2xl:px-6'>
+         <div class='container'>
+            <div class='p-8 overflow-hidden bg-white shadow-cardShadow rounded-md max-w-xl mx-auto'>
+               <h4 class='mb-8 text-xl font-bold text-center'>Add New Book</h4>
+               <Form mode='add' />
+            </div>
+         </div>
+      </main>
+   );
 }
