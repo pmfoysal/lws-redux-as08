5a6e6d698de2a0e9ff286f14aeab28e106b93a95
pmfoysal <pmfoysal@gmail.com>
2023-03-18 21:37:52 +0600

    Book Search Input Functionality Added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,19 @@
 import { Link, useLocation } from 'react-router-dom';
+import { setSearch } from '../redux/features/others';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Header() {
+   const dispatch = useDispatch();
    const { pathname } = useLocation();
+   const { search } = useSelector(store => store.others);
 
    function getColor(path) {
       if (path === pathname) return 'font-semibold';
       return '';
+   }
+
+   function handleSearch(event) {
+      dispatch(setSearch(event.target.value));
    }
 
    return (
@@ -20,7 +28,7 @@ export default function Header() {
                   <li>Add Book</li>
                </Link>
             </ul>
-            <form className='flex items-center'>
+            <form className='flex items-center' onSubmit={e => e.preventDefault()}>
                <div className='group relative rounded-md bg-white'>
                   <svg
                      width='20'
@@ -34,7 +42,14 @@ export default function Header() {
                         d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
                      ></path>
                   </svg>
-                  <input type='text' placeholder='Filter books...' className='search' id='lws-search' />
+                  <input
+                     type='text'
+                     placeholder='Filter books...'
+                     className='search'
+                     id='lws-search'
+                     value={search}
+                     onChange={handleSearch}
+                  />
                </div>
             </form>
          </div>
